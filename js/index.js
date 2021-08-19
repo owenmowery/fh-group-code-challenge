7734d6ed6690ecfa31761e15d8c39acb5dd26dd5
Owen Mowery <owenmowery99@gmail.com>
2021-08-19 17:33:13 -0400

    albums now rendering on screen

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
     API.addSongs();
+    console.log(Album.all)
 })
--- js/models/album.js
+++ js/models/album.js
@@ -10,6 +10,28 @@ class Album {
         this.link = link;
         this.price = price;
         this.date = date;
+
+        this.renderAlbum();
+    }
+
+    renderAlbum() {
+        const albumHolder = document.getElementById('album-collection');
+        const albumContainer = document.createElement('div');
+        //albumContainer.dataset.id = this.id;
+        //albumContainer.id = this.id
+        albumContainer.setAttribute("class", "album-container");
+        albumContainer.innerHTML += this.showHTML();
+        albumHolder.appendChild(albumContainer);
+    }
+
+    showHTML() {
+        return ` 
+        <img src=${this.image} alt="album cover image">
+        <h3>${this.title}</h3>
+        <h4>${this.artist}</h4>
+        <p>Price: ${this.price}</p>
+        <p>Release Date: ${this.date}</p>
+        `
     }
 
 }
