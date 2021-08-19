e7aa1696637bfa7f29ccbfa6b82938be80b64674
Owen Mowery <owenmowery99@gmail.com>
2021-08-19 18:27:27 -0400

    albums now render in cols and rows on browser, images blurry

--- js/models/album.js
+++ js/models/album.js
@@ -17,8 +17,6 @@ class Album {
     renderAlbum() {
         const albumHolder = document.getElementById('album-collection');
         const albumContainer = document.createElement('div');
-        //albumContainer.dataset.id = this.id;
-        //albumContainer.id = this.id
         albumContainer.setAttribute("class", "album-container");
         albumContainer.innerHTML += this.showHTML();
         albumHolder.appendChild(albumContainer);
@@ -27,7 +25,7 @@ class Album {
     showHTML() {
         return ` 
         <a href=${this.link}><img src=${this.image} alt="album cover image"></a>
-        <h3>${this.title}</h3>
+        <h3>${this.title.slice(0, 20)}..</h3>
         <h4>${this.artist}</h4>
         <p>Price: ${this.price}</p>
         <p>Release Date: ${this.date}</p>
