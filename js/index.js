98419c767f09556518d889048c93b26506d3b0e9
Owen Mowery <owenmowery99@gmail.com>
2021-08-21 20:42:20 -0400

    added search feature

--- js/index.js
+++ js/index.js
@@ -1,4 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
     API.addSongs();
     console.log(Album.all)
+
+    const searchBar = document.getElementById("search-bar");
+    searchBar.addEventListener("keyup", e => {
+        const searchString = e.target.value.toLowerCase();
+        console.log(searchString);
+
+        const filteredAlbums = Album.all.filter( album => {
+            return (
+                album.title.toLowerCase().includes(searchString)
+            );
+        })
+
+        console.log(filteredAlbums);
+
+        document.getElementById("album-collection").innerHTML = "";
+        filteredAlbums.forEach(album => {
+            album.renderAlbum();
+        })
+    })
 })
